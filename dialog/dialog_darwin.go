4ea1ab4034fd024e8c95acc341b7d871fff5cfef
Mindaugas Mečėjus <mindaugas.mecejus@gmail.com>
2026-05-12 19:46:54 +0200

    Refactor dialog functions to remove icon parameters and simplify dialog display

--- dialog/dialog_darwin.go
+++ dialog/dialog_darwin.go
@@ -10,18 +10,18 @@ const title = "whisprgo"
 
 // Error displays a blocking error dialog with the given message.
 func Error(message string) {
-	show(message, "stop")
+	show(message)
 }
 
 // Info displays a blocking informational dialog with the given message.
 func Info(message string) {
-	show(message, "note")
+	show(message)
 }
 
 // Prompt shows a text-entry dialog and returns the user's input. The second
 // return is false if the user cancelled or osascript failed.
 func Prompt(message string) (string, bool) {
-	const script = `set r to display dialog (system attribute "WHISPRGO_MSG") with title (system attribute "WHISPRGO_TITLE") default answer "" with icon note
+	const script = `set r to display dialog (system attribute "WHISPRGO_MSG") with title (system attribute "WHISPRGO_TITLE") default answer ""
 return text returned of r`
 	cmd := exec.Command("osascript", "-e", script)
 	cmd.Env = append(os.Environ(),
@@ -35,8 +35,8 @@ return text returned of r`
 	return strings.TrimRight(string(out), "\n"), true
 }
 
-func show(message, icon string) {
-	script := `display dialog (system attribute "WHISPRGO_MSG") with title (system attribute "WHISPRGO_TITLE") buttons {"OK"} default button "OK" with icon ` + icon
+func show(message string) {
+	const script = `display dialog (system attribute "WHISPRGO_MSG") with title (system attribute "WHISPRGO_TITLE") buttons {"OK"} default button "OK"`
 	cmd := exec.Command("osascript", "-e", script)
 	cmd.Env = append(os.Environ(),
 		"WHISPRGO_MSG="+message,
